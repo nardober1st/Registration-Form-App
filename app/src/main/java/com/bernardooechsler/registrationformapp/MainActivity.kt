3d7f38b36f938dce65d0faa40c278f456cae10f1
nardober1st <bernardo.oechsler6@gmail.com>
2023-03-22 23:30:28 -0700

    Final Commit

---
+++ app/src/main/java/com/bernardooechsler/registrationformapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.bernardooechsler.registrationformapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupSpinner()
+        setupButton()
+    }
+
+    private fun setupSpinner() {
+        val titles = arrayOf("Miss", "Mrs", "Ms", "Mr", "Mx", "Dr", "Prof")
+        val titleAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, titles)
+        spinner_title.adapter = titleAdapter
+    }
+
+    private fun setupButton() {
+        button_create_account.setOnClickListener {
+            createAccount()
+        }
+    }
+
+    private fun createAccount() {
+        val user = User(
+            spinner_title.selectedItem as String,
+            edit_text_first_name.text.toString(),
+            edit_text_last_name.text.toString(),
+            edit_text_email.text.toString(),
+            edit_text_phone_number.text.toString(),
+            edit_text_password.text.toString()
+        )
+        val intent = Intent(this, SummaryActivity::class.java)
+        intent.putExtra("User", user)
+        startActivity(intent)
+    }
+}
